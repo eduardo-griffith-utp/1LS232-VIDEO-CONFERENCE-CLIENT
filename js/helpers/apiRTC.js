f1369e3ec17c12b682eb1e92654a570917d2b0aa
eduardo-griffith-utp <163909552+eduardo-griffith-utp@users.noreply.github.com>
2024-05-29 16:18:20 -0500

    Fix para apiRTC await

--- js/helpers/apiRTC.js
+++ js/helpers/apiRTC.js
@@ -1,6 +1,7 @@
 class ApiRTCHelper {
     static API_KEY = CONFIG.apiRTC.apiKey;
-    static userAgent = new apiRTC.UserAgent({ uri: 'apiKey:' + this.API_KEY });
+    static userAgent = undefined;
+    static session = undefined;
     static conversation = undefined;
     static streamList = [];
     static localStream = null;
@@ -10,77 +11,14 @@ class ApiRTCHelper {
         this.room = room;
         
         try {
-            var userAgent = new apiRTC.UserAgent({ uri: 'apiKey:' + CONFIG.apiRTC.apiKey })
-            let conversation = undefined;
+            this.userAgent = new apiRTC.UserAgent({ uri: 'apiKey:' + CONFIG.apiRTC.apiKey })
 
-            userAgent.register({
-                cloudUrl: 'https://cloud.apirtc.com'
-            }).then( (session) => {    
-                conversation = session.getOrCreateConversation(self.room);
-                    
-                // Define the way the local stream will be configured
-                let streamOptions = {
-                    constraints: {
-                        audio: true,
-                        video: true
-                    }
-                }
-    
-                //Get the stream for the local camera
-                userAgent.createStream(streamOptions).then(function (stream) {
-                    self.localStream = stream;
-    
-                    //Display the stream (audio+video) in the <video> element with id 'local'
-                    var element = document.getElementById('publisher-video');
-                    console.log(element);
-                    stream.attachToElement(element)
-                    
-                    //Join the conversation
-                    conversation.join().then(() => {
-                    
-                        //Publish the local stream in the conversation
-                        conversation.publish(stream)
-                    });
-                });  
-                
-                //Any time the streamListChanged is triggered
-                conversation.on('streamListChanged', (streamInfo) => {
-    
-                    // when a new stream is added AND the added stream is not 
-                    if (streamInfo.listEventType === 'added' && streamInfo.isRemote === true) {
-                        onStreamAdded(streamInfo);
-
-                        //Subscribe to the new stream's events (triggered by the conversation object)
-                        conversation.subscribeToMedia(streamInfo.streamId).then((stream) => {
-                            console.log("Subscribed to " + stream.streamId)
-                        })
-                    }
-                });
-    
-                //Any time a (subscribed) stream is added to the conversation
-                conversation.on('streamAdded', (stream) => {
-                    
-                    //Add a video element in the 'remote' div element,  with the streamId in its id attribute
-                    stream.addInDiv('remote-' + stream.streamId, 'remote-media-' + stream.streamId, {}, false)
-    
-                });
-    
-                //Any time a (subscribed) stream is removed from the conversation
-                conversation.on('streamRemoved', (stream) => {
-                    
-                    //Remove a video element with the streamId in its id attribute
-                    // stream.removeFromDiv('remote', 'remote-media-' + stream.streamId)
-                    onStreamRemoved(stream);
-                }); 
-            });
- 
-            /*
-            const session = await this.userAgent.register({
+            this.session = await this.userAgent.register({
                 cloudUrl: CONFIG.apiRTC.cloudUrl
             });
                 
             // Preparar conexión a la conversación
-            // this.conversation = session.getOrCreateConversation(this.room);
+            this.conversation = this.session.getOrCreateConversation(this.room);
 
             // Cada vez que se desencadena el evento streamListChanged
             this.conversation.on('streamListChanged', (streamInfo) => {
@@ -89,7 +27,7 @@ class ApiRTCHelper {
                     onStreamAdded(streamInfo);
 
                     // Suscribirse a los eventos del nuevo stream (desencadenados por el objeto conversación)
-                    this.conversation.subscribeToMedia(streamInfo.streamId).then((stream) => {
+                    self.conversation.subscribeToMedia(streamInfo.streamId).then((stream) => {
                         console.log("Suscrito a " + stream.streamId);
                     });
                 }
@@ -118,18 +56,18 @@ class ApiRTCHelper {
             };
 
             // Obtener el stream de la cámara local
-            const stream = await this.userAgent.createStream(streamOptions);
+            this.localStream = await this.userAgent.createStream(streamOptions);
             
             // Mostrar el stream (audio+video) en el elemento <video> con id 'publisher-video'
             var element = document.getElementById('publisher-video');
-            stream.attachToElement(element);
+            this.localStream.attachToElement(element);
             
             // Unirse a la conversación
             await this.conversation.join();
             
             // Publicar el stream local en la conversación
-            await this.conversation.publish(stream);
-            */
+            await this.conversation.publish(this.localStream);
+        
         } catch (error) {
             console.error('Error durante la conexión:', error);
         }
