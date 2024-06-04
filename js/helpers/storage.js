9acc2660cdd2768c23a0cf1d3626d1149608fc29
Humberto Ossa <titoossa29@gmail.com>
2024-06-03 20:33:54 -0500

    task creacion de archivo storage.js

---
+++ js/helpers/storage.js
@@ -0,0 +1,20 @@
+
+class storagehelper{
+
+  static getFiles(path){
+
+  return ["uploads/file1.txt", "uploads/file2.jpg", "uploads/file3.pdf"];
+  }
+
+  static upload(file, path, progress_callback) {
+  
+    return true
+  }
+
+  static download (file, path) {
+
+    return "https://google.com";
+  }
+
+}
+
