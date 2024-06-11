4e0dc1464c563a3e1241975c0002cf32b2bdebae
Ghadafy Neville <kadaneville@gmail.com>
2024-06-10 22:00:12 -0500

    [ARCHIVOS] - Implementar el método download en StorageHelper

--- js/helpers/storage.js
+++ js/helpers/storage.js
@@ -1,20 +1,37 @@
 
-class StorageHelper{
+class StorageHelper {
 
-  static getFiles(path){
+  static getFiles(path) {
 
-  return ["uploads/file1.txt", "uploads/file2.jpg", "uploads/file3.pdf"];
+    return ["uploads/file1.txt", "uploads/file2.jpg", "uploads/file3.pdf"];
   }
 
   static upload(file, path, progress_callback) {
-  
+
     return true
   }
 
-  static download (file, path) {
+  // Método download
+  static download(file, path) {
 
-    return "https://google.com";
+    return new Promise((resolve, reject) => {
+
+      const storageRef = firebase.storage().ref(path);
+
+      storageRef.getDownloadURL()
+        .then((url) => {
+
+          resolve(url);
+        })
+        .catch((error) => {
+
+          reject(error);
+        });
+    });
+
+
   }
+
 
 }
 
