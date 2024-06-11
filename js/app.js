ca977ab5d9f5cc6745bc96f4fe9095bce0c22bc5
josevega24 <143565875+josevega24@users.noreply.github.com>
2024-06-10 21:57:57 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,117 +1,107 @@
-
 const App = {
     mode: "light",
     view: "call",
-    
     userName: null,
     room: null,
     roomName: null,
-
-  
-
-    video: true, 
+    video: true,
     audio: true,
-
-
-
     message: "",
-
     users: [],
     streamList: [],
     chats: [],
     files: [],
     notes: [],
-
     CallActions: new CallActions(),
 
     toggleMode() {
-        if (this.mode == "light") {
-            this.mode = "dark";
-        } else {
-            this.mode = "light";
-        }
+        this.mode = this.mode === "light" ? "dark" : "light";
     },
+
     async accessRoom() {
         let self = this;
         this.room = this.roomName;
         this.roomName = null;
 
         await AblyHelper.connect(this.room, (message) => {
-            console.log('Received a message in realtime: ' + message.data)
+            console.log('Received a message in realtime: ' + message.data);
             var json = JSON.parse(message.data);
-            switch (json.action) { 
-                case "user":         
-                    if (
-                        json.streamId != ApiRTCHelper.localStream.publishedInConversations.get(this.room) &&
-                        !this.streamList.find(stream => stream.streamId == json.streamId)                        
-                    ) {
+            switch (json.action) {
+                case "user":
+                    if (json.streamId != ApiRTCHelper.localStream.publishedInConversations.get(this.room) &&
+                        !this.streamList.find(stream => stream.streamId == json.streamId)) {
                         this.streamList.push(json);
-                    }      
+                    }
                     break;
                 case "chat":
                     self.chats.push(json);
                     break;
                 case "file":
-                    self.chats.push(json); 
+                    self.chats.push(json);
                     self.files.push(json.file);
-                    break
+                    break;
             }
         });
 
         await ApiRTCHelper.connect(
             this.room,
-            async (stream) => {              
-                await AblyHelper.send({ 
-                    action: "user", 
-                    user: this.userName, 
-                    streamId: ApiRTCHelper.localStream.publishedInConversations.get(this.room) 
-                });  
+            async (stream) => {
+                await AblyHelper.send({
+                    action: "user",
+                    user: this.userName,
+                    streamId: ApiRTCHelper.localStream.publishedInConversations.get(this.room)
+                });
             },
             (stream) => {
                 this.streamList = this.streamList.filter(x => x.streamId != stream.streamId);
             }
         );
     },
+
     async sendMessage() {
         await this.sendChat({
-            "action": "chat",
-            "message": this.message,            
+            action: "chat",
+            message: this.message,
         });
         this.message = '';
     },
 
     async sendChat(chat) {
         chat.sender = {
-            "name": this.userName,
-            "picture": "images/avatar.jpeg"
-        }
-        //almacenando el chat 
+            name: this.userName,
+            picture: "images/avatar.jpeg"
+        };
         await DatabaseHelper.saveChat(chat);
         await AblyHelper.send(chat);
     },
- 
+
     toggleAudio() {
         this.audio = !this.audio;
         ApiRTCHelper.toggleAudio();
     },
+
     toggleVideo() {
-        this.video = !this.video; 
+        this.video = !this.video;
         ApiRTCHelper.toggleVideo();
     },
 
-    upload(file){
-        const result = StorageHelper.upload(file, `${this.room/file.name}`)
-        if(result) {
+    async upload(file) {
+        const result = await StorageHelper.upload(file, `${this.room}/${file.name}`);
+        if (result) {
             this.sendChat({
-                "action": "file",
-                "file": `${this.room/file.name}`
-            })
+                action: "file",
+                file: `${this.room}/${file.name}`
+            });
         }
+    },
+
+    download(file) {
+        StorageHelper.download(file);
     }
 };
 
 document.addEventListener('alpine:init', () => {
-    Alpine.data('App', () => (App))
+    Alpine.data('App', () => (App));
 });
 
 window.ondragover = function (event) {
@@ -122,9 +112,8 @@ window.ondrop = async function (event) {
     event.preventDefault();
     const files = event.dataTransfer.files;
     for (const file of files) {
-        upload(file)
+        App.upload(file);
     }
 };
 
 firebase.initializeApp(CONFIG.Firebase);
-
