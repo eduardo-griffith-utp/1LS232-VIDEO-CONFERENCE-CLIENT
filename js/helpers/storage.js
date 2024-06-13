23f6a2b5bc539b30a47035d6df95a75b6917c77b
Abrego-Gerardo <143464224+Abrego-Gerardo@users.noreply.github.com>
2024-06-13 23:34:01 +0000

    StorageHelper

--- js/helpers/storage.js
+++ js/helpers/storage.js
@@ -6,9 +6,38 @@ class StorageHelper{
   return ["uploads/file1.txt", "uploads/file2.jpg", "uploads/file3.pdf"];
   }
 
-  static upload(file, path, progress_callback) {
-  
-    return true
+  static async upload(file, path, progress_callback) {
+    path = path + "/" + file.name;
+    return new Promise((resolve, reject) => {
+      const storageRef = firebase.storage().ref(updatedPath);
+      const uploadTask = storageRef.put(file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          if (progress_callback) {
+            progress_callback(progress);
+          }
+
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED:
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING:
+              console.log('Upload is running');
+              break;
+          }
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
   }
 
   static download (file, path) {
