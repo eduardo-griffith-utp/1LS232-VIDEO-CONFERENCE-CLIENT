bbf223ab3b7d7b0a23f0a1292fb49107b54bd918
ryanster455 <142760926+ryanster455@users.noreply.github.com>
2024-06-13 18:27:00 -0600

    Revert "Update call-actions.js"
    This reverts commit fa7d22517d9af2dedb8ead425a5643a9eaa43d49.

--- js/classes/call-actions.js
+++ js/classes/call-actions.js
@@ -1,31 +1,13 @@
 class CallActions {
-    constructor() {
-        this.audio = false; // Propiedad para almacenar el estado del audio
-    }
-
     toggleAudio() {
-        this.audio = !this.audio; // Cambiar el estado del audio
-        return this.audio; // Devolver el nuevo estado del audio
-    }
-
-    toggleVideo() {
         return Math.random() < 0.5;
     }
 
-    leaveConversation(message) {
-        alert(message || "To be implemented");
+    toggleVideo() {
+        return Math.random < 0.5;
+    }
+
+    leaveConversation (message) {
+        alert (message || "To be implement")
     }
 }
-
-// Crear una instancia de CallActions
-const callActions = new CallActions();
-
-// Obtener el botón de audio por su id
-const audioButton = document.getElementById('audioButton');
-
-// Agregar un event listener para el evento "click" al botón de audio
-audioButton.addEventListener('click', function() {
-    // Llamar al método toggleAudio de CallActions y asignar el resultado a la propiedad audio
-    callActions.audio = callActions.toggleAudio();
-    console.log("Audio activado:", callActions.audio);
-});
