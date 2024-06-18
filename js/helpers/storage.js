e1294c387e4f064c221d95e10dc6c384d064185c
Abrego-Gerardo <143464224+Abrego-Gerardo@users.noreply.github.com>
2024-06-17 21:24:21 -0500

    Update storage.js

--- js/helpers/storage.js
+++ js/helpers/storage.js
@@ -6,37 +6,41 @@ class StorageHelper{
   return ["uploads/file1.txt", "uploads/file2.jpg", "uploads/file3.pdf"];
   }
 
-  static async upload(file, path, progress_callback) {
-    path = path + "/" + file.name;
+  static upload(file, path, progress_callback) {
+    const updatedPath = path + "/" + file.name;
+  
     return new Promise((resolve, reject) => {
-      const storageRef = firebase.storage().ref(updatedPath);
-      const uploadTask = storageRef.put(file);
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          if (progress_callback) {
-            progress_callback(progress);
-          }
+        const storageRef = firebase.storage().ref(updatedPath);
 
-          switch (snapshot.state) {
-            case firebase.storage.TaskState.PAUSED:
-              console.log('Upload is paused');
-              break;
-            case firebase.storage.TaskState.RUNNING:
-              console.log('Upload is running');
-              break;
-          }
-        },
-        (error) => {
-          reject(error);
-        },
-        () => {
-          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-            resolve(downloadURL);
-          });
-        }
-      );
+        const uploadTask = storageRef.put(file);
+
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+                if (progress_callback) {
+                    progress_callback(progress);
+                }
+  
+                switch (snapshot.state) {
+                    case firebase.storage.TaskState.PAUSED:
+                        console.log('Upload is paused');
+                        break;
+                    case firebase.storage.TaskState.RUNNING:
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
+                reject(error);
+            },
+            () => {
+                uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                    resolve(downloadURL);
+                });
+            }
+        );
     });
   }
 
