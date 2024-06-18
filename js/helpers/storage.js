b337322fef85c049b3d38bc31d24fc337cfe3611
Abrego-Gerardo <143464224+Abrego-Gerardo@users.noreply.github.com>
2024-06-17 21:47:23 -0500

    Update storage.js

--- js/helpers/storage.js
+++ js/helpers/storage.js
@@ -7,40 +7,37 @@ class StorageHelper{
   }
 
   static upload(file, path, progress_callback) {
-    const updatedPath = path + "/" + file.name;
-
+    path = path + "/" + file.name; 
     return new Promise((resolve, reject) => {
-        const storageRef = firebase.storage().ref(updatedPath);
+      const storageRef = firebase.storage().ref(updatedPath);
+      const uploadTask = storageRef.put(file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 
-        const uploadTask = storageRef.put(file);
+          if (progress_callback) {
+            progress_callback(progress);
+          }
 
-        uploadTask.on(
-            'state_changed',
-            (snapshot) => {
-                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-
-                if (progress_callback) {
-                    progress_callback(progress);
-                }
-
-                switch (snapshot.state) {
-                    case firebase.storage.TaskState.PAUSED:
-                        console.log('Upload is paused');
-                        break;
-                    case firebase.storage.TaskState.RUNNING:
-                        console.log('Upload is running');
-                        break;
-                }
-            },
-            (error) => {
-                reject(error);
-            },
-            () => {
-                uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-                    resolve(downloadURL);
-                });
-            }
-        );
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED:
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING:
+              console.log('Upload is running');
+              break;
+          }
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
     });
   }
 
