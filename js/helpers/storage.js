4a65af2a01261c8122b2909ce646cb71b02a59e9
Ghadafy Neville <kadaneville@gmail.com>
2024-06-17 21:47:44 -0500

    Archivo - Metodo de Dowload2

--- js/helpers/storage.js
+++ js/helpers/storage.js
@@ -1,32 +1,48 @@
 
-class StorageHelper{
+class StorageHelper {
 
   static getFiles(path) {
     return new Promise((resolve, reject) => {
-      
+
       const storageRef = firebase.storage().ref(path);
-  
+
       storageRef.listAll()
         .then(res => {
-        
+
           const files = res.items.map(itemRef => itemRef.fullPath);
           resolve(files);
         })
         .catch(error => {
-         
+
           reject(error);
         });
     });
   }
 
-  static upload(file, path, progress_callback) {  
+  static upload(file, path, progress_callback) {
     return true
   }
 
-  static download (file, path) {
 
-    return "https://google.com";
+  static download(file, path) {
+
+    return new Promise((resolve, reject) => {
+
+      const storageRef = firebase.storage().ref(path);
+
+      storageRef.getDownloadURL()
+        .then((url) => {
+
+          resolve(url);
+        })
+        .catch((error) => {
+
+          reject(error);
+        });
+    });
+
   }
+
 
 }
 
